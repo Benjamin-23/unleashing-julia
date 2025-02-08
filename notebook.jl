ff2cd70999a7e5c109e94448a66a1df1acbca320
benjamin-23 <kitongabenja34@gmail.com>
2025-02-08 03:10:18 +0300

    julia magic

---
+++ notebook.jl
@@ -0,0 +1,11 @@
+### A Pluto.jl notebook ###
+# v0.20.4
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 0cc6ce20-e5b0-11ef-3f99-45da6f3448d0
+println("Hello, World!")
+
+# ╔═╡ Cell order:
+# ╠═0cc6ce20-e5b0-11ef-3f99-45da6f3448d0
